5e2606e7e689d0ee92d6f83d3a439deee17bc5c8
RikoBato <s20c1101jd@s.chibakoudai.jp>
2021-05-01 04:37:41 +0900

    3

--- test.c
+++ test.c
@@ -7,9 +7,12 @@ int main(){
   printf("2");
   }
   gpioSetMode(26,PI_INPUT);
-  while(1){
+  /*while(1){
     //gpioWrite(26,0);
     gpioPWM(26,100);
+  }*/
+  for(int i=0;i<255;i=i+i*10){
+  gpioPWM(26,i);
   }
   return 0;
 }
