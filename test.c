18ad170cd242121c44992dd80125237f75af67a3
RikoBato <s20c1101jd@s.chibakoudai.jp>
2021-05-01 05:07:44 +0900

    3

--- test.c
+++ test.c
@@ -7,11 +7,21 @@ int main(){
   printf("2");
   }
   gpioSetMode(26,PI_INPUT);
-  int i=0;
+  int i=10;
+  int d=0;
+  
   while(1){
+    gpioPWM(26,i);
+    gpioSleep(PI_TIME_RELATIVE,0,500000);
+    d+=r;
+    if(d>=255||d<=0){
+      r*=-1;
+    }
+  }
+  /*while(1){
     //gpioWrite(26,0);
     gpioPWM(26,i);
-    if(i>250){
+    if(i=250){
     i=i-250;
     }
     gpioSleep(PI_TIME_RELATIVE,0,500000);
