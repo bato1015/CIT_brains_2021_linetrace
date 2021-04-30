bbf2fdc957ce4f4dd852fe5d6c0156262899c6d4
RikoBato <s20c1101jd@s.chibakoudai.jp>
2021-05-01 03:15:06 +0900

    3

--- LED1.c
+++ LED1.c
@@ -1,8 +1,12 @@
 #include<pigpio.h>
 #include<stdio.h>
 int outputpin=17;
+int PI_OITPUT=1;
 int main(){
-  int gpioinitialise(void);
+  if (gpioInitialise() < 0){
+  }else{
+  }
   int gpioSetMode(outputpin,PI_OUTPUT);
+  gpioTerminate();
   return 0;
 }
